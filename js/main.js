9f624f2b5ac70a6e9b89940990e06fdf41f7a9ce
James Pants <jamespants@gmail.com>
2014-01-08 16:48:26 +0100

    add final film, ready to deploy

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ Video background not fading out on video play button click
 										'seamless="seamless" webkitallowfullscreen="webkitAllowFullScreen" ' +
 										'mozallowfullscreen="mozallowfullscreen" allowfullscreen="allowfullscreen" ' +
 										'id="rbmaplayer" width="' + width + '" height="' + height + '" ' +
-										'src="http://www.youtube.com/embed/8dtZsnPKeo8?enablejsapi=1&amp;origin=*&amp;autoplay=1&amp;loop=1&amp;hd=1&amp;' +
+										'src="http://www.youtube.com/embed/uVzx_p_DrL8?enablejsapi=1&amp;origin=*&amp;autoplay=1&amp;loop=1&amp;hd=1&amp;' +
 										'controls=1&amp;showinfo=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;' +
 										'rel=0&amp;playlist=O8hi7CaqE8A"></iframe>');
 		}
@@ -57,7 +57,7 @@ Video background not fading out on video play button click
 										'seamless="seamless" webkitallowfullscreen="webkitAllowFullScreen" ' +
 										'mozallowfullscreen="mozallowfullscreen" allowfullscreen="allowfullscreen" ' +
 										'id="rbmaplayer" width="' + width + '" height="' + height + '" ' +
-										'src="http://www.youtube.com/embed/8dtZsnPKeo8?enablejsapi=1&amp;origin=*&amp;autoplay=1&amp;loop=1&amp;hd=1&amp;' +
+										'src="http://www.youtube.com/embed/uVzx_p_DrL8?enablejsapi=1&amp;origin=*&amp;autoplay=1&amp;loop=1&amp;hd=1&amp;' +
 										'controls=0&amp;showinfo=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;' +
 										'rel=0&amp;playlist=O8hi7CaqE8A"></iframe>');
 		}
